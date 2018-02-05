95a884716800e58d8b05d3d24572ea1e55f3abdd
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2018-02-05 14:23:12 +0000

    Created cookbook

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,14 +5,25 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+# Nginx
+describe package('nginx') do
+	it {should be_installed}
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe service('nginx') do
+	it {should be_running}
+	it {should be_enabled}
+end
+
+describe port(80) do
+  	it { should be_listening }
+end
+
+describe http('http://localhost', enable_remote_worker: true) do
+	its('status') {should cmp 502}
+end
+
+describe package('nodejs') do
+	it {should be_installed}
+	its ('version') {should match /6\../}
 end
